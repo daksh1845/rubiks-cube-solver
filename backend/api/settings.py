2eb01e97f2fd0687c8c0c533873534dde5718bbd
Daksh <dakshmalik1845@gmail.com>
2026-04-07 10:47:52 +0530

    updated CORS_ALLOWED_ORIGINS

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -53,7 +53,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = ['*']
+CORS_ALLOWED_ORIGINS = [
+    "https://rcsweb.vercel.app",
+]
 
 ROOT_URLCONF = 'api.urls'
 
